cc6b0dba2bf88bd35544263c4683391a150f6f18
Lars-Christian Fürstenberg <lcf@miztli.de>
2020-11-22 11:31:34 +0100

    More block iterator tests

--- test/block_iterator_test.cpp
+++ test/block_iterator_test.cpp
@@ -38,50 +38,21 @@
 
 namespace
 {
-  class MyBlockProvider : public csvsqldb::BlockProvider
+  class MockBlockProvider : public csvsqldb::BlockProvider
   {
   public:
-    MyBlockProvider(csvsqldb::BlockManager& manager)
-    : _manager{manager}
+    MockBlockProvider(std::function<csvsqldb::BlockPtr()> provider)
+    : _provider{std::move(provider)}
     {
-      _block = _manager.createBlock();
     }
 
     csvsqldb::BlockPtr getNextBlock() override
     {
-      switch (_count) {
-        case 0:
-          _block->addInt(4711, false);
-          _block->addBool(true, false);
-          _block->addDate(csvsqldb::Date{2020, csvsqldb::Date::November, 21}, false);
-          _block->nextRow();
-
-          _block->addInt(42, false);
-          _block->addBool(false, false);
-          _block->addDate(csvsqldb::Date{}, true);
-          _block->nextRow();
-          _block->markNextBlock();
-          ++_count;
-          break;
-        case 1:
-          _block = _manager.createBlock();
-          _block->addInt(815, false);
-          _block->addBool(false, false);
-          _block->addDate(csvsqldb::Date{1970, csvsqldb::Date::May, 17}, false);
-          _block->nextRow();
-          _block->endBlocks();
-          break;
-        default:
-          _block = nullptr;
-      }
-
-      return _block;
+      return _provider();
     }
 
   private:
-    uint8_t _count{0};
-    csvsqldb::BlockManager& _manager;
-    csvsqldb::BlockPtr _block{nullptr};
+    std::function<csvsqldb::BlockPtr()> _provider;
   };
 }
 
@@ -90,10 +61,43 @@ TEST_CASE("Block Iterator Test", "[block]")
 {
   csvsqldb::Types types{csvsqldb::INT, csvsqldb::BOOLEAN, csvsqldb::DATE};
   csvsqldb::BlockManager manager;
-  MyBlockProvider provider{manager};
 
   SECTION("next row")
   {
+    MockBlockProvider provider{[&manager]() {
+      static csvsqldb::BlockPtr block{manager.createBlock()};
+      static uint16_t count{0};
+
+      switch (count) {
+        case 0:
+          block->addInt(4711, false);
+          block->addBool(true, false);
+          block->addDate(csvsqldb::Date{2020, csvsqldb::Date::November, 21}, false);
+          block->nextRow();
+
+          block->addInt(42, false);
+          block->addBool(false, false);
+          block->addDate(csvsqldb::Date{}, true);
+          block->nextRow();
+          block->markNextBlock();
+          ++count;
+          break;
+        case 1:
+          block = manager.createBlock();
+          block->addInt(815, false);
+          block->addBool(false, false);
+          block->addDate(csvsqldb::Date{1970, csvsqldb::Date::May, 17}, false);
+          block->nextRow();
+          block->endBlocks();
+          ++count;
+          break;
+        default:
+          block = nullptr;
+      }
+
+      return block;
+    }};
+
     csvsqldb::BlockIterator iter{types, provider, manager};
 
     auto* values = iter.getNextRow();
@@ -119,5 +123,25 @@ TEST_CASE("Block Iterator Test", "[block]")
 
     values = iter.getNextRow();
     CHECK_FALSE(values);
+  }
+  SECTION("missing row delimiter")
+  {
+    MockBlockProvider provider{[&manager]() {
+      static csvsqldb::BlockPtr block{manager.createBlock()};
+
+      block->addInt(4711, false);
+      block->addBool(true, false);
+      block->addDate(csvsqldb::Date{2020, csvsqldb::Date::November, 21}, false);
+      block->endBlocks();
+
+      return block;
+    }};
+
+    csvsqldb::BlockIterator iter{types, provider, manager};
+
+    const auto* values = iter.getNextRow();
+    REQUIRE(values);
+    REQUIRE(3 == values->size());
+    CHECK_THROWS_AS(iter.getNextRow(), csvsqldb::Exception);
   }
 }
