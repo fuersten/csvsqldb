8c28d6ada73e3d75c544b78695d4abb6dbc2955b
Lars-Christian Fürstenberg <lcf@miztli.de>
2021-04-12 21:15:31 +0200

    Lua will now be fetched and build inside of the project

---
+++ cmake/fetch_lua.cmake
@@ -0,0 +1,10 @@
+FetchContent_Declare(
+  lua
+  URL https://github.com/lua/lua/archive/refs/tags/v5.4.3.tar.gz
+)
+
+FetchContent_GetProperties(date)
+if(NOT lua_POPULATED)
+  FetchContent_Populate(lua)
+endif()
+
